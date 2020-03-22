fddc4ec418499a1cdea886213dc29f9d87597b65
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-03-22 22:51:26 +0900

    Update p4564.c

--- p4564.c
+++ p4564.c
@@ -5,20 +5,35 @@
     int num1;
     int arr[100];
 
+    #include <stdio.h>
+#include <stdlib.h>
+
+    int d[1000];
+    int num1;
+    int arr[1000];
+
     int max(int a,int b)
     {
-        return a>b ? a: b;
+       if(a>b)
+       {
+           return a;
+       }
+       else
+       {
+           return b;
+       }
     }
-    int up(int n)
-    {
-        if(n<0) return 0;
 
+int up(int n)
+{
+        if(n<0) return 0;
         if(n==1) return d[1]=arr[1];
         if(n==2) return d[2]=arr[1]+arr[2];
         if(d[n]!=0) return d[n];
-        else return d[n]=max(up(n-3)+arr[n-1],up(n-2)+arr[n]);
-    }
+        else return d[n]=max(up(n-3)+arr[n-1], up(n-2))+arr[n];
 
+
+}
 
     int main(){
     freopen("input.txt","r",stdin);
@@ -27,6 +42,8 @@
     for(int i=1; i<=num1;i++){
     scanf("%d", &arr[i]);
     }
+
+
     printf("%d",up(num1));
 
 }
