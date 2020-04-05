bfaab20551489eeb802fe51ce34cfffa96846030
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-04-05 11:58:29 +0900

    Add files via upload

---
+++ p0405.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void main(){
+int num1,num2[100],num=0;
+freopen("input.txt","r",stdin);
+scanf("%d", &num1);
+
+for(int i=0; i<5; i++)
+{
+    scanf("%d", &num2[i]);
+}
+int k=num1%10; //날짜의 일의 자리수
+
+
+
+for(int i=0; i<5; i++){
+   if(k != num2[i]) //번호의 일의 자리 수
+   {
+    continue;
+   }
+   num++;
+
+}
+
+printf("%d", num);
+}
+
+
