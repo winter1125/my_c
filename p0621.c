f5a6adb61271845e4f7cb88c92e6465c8872c16c
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-06-21 02:00:26 +0900

    Add files via upload

---
+++ p0621.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
+
+void main()
+{
+	char mun[] = "computer architure";
+	int cnt, a, b, top = 0;
+
+	for (a = 0; a <= 4; a++)
+	{
+		cnt = 0;
+		for (b = a; b <= 21; b++)
+		{
+			if(mun[a] == mun[b])
+				cnt = cnt + 1;
+		}
+		if (cnt > top)
+			top = cnt;
+		printf("%d", top);
+	}
+	//printf("%d\n", top);
+
+}
