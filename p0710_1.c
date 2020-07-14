218a545b3e31a437b5ccdc622131c24e67ca4804
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-07-14 19:32:11 +0900

    Add files via upload

--- p0710_1.c
+++ p0710_1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 //#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
+#define SQ1(n)n*n
+#define SQ2(n)(n*n)
+#define SQ3(n)((n)*(n))
 
-
-int sum(int n) {
-	printf("%d\n", n);
-	if (n < 1) return 1;
-	else return (n + sum(n - 1));
-}
-void main()
-{
-	printf("sum=%d", sum(5));
+main() {
+	int i = 0, k;
+	while (i <= 5)
+	{
+		k = i;
+		printf("i=%d %5d %5d %5d\n", i, SQ1(i+1), SQ2(++i), SQ3(k + 1))
+	}
 }
