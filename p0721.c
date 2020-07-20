6fde349f31df11c0a9889f50b7e796a858636ac7
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-07-21 07:18:40 +0900

    Add files via upload

---
+++ p0721.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+double power(int x, int y){
+return x <<x);
+	//return 
+}
+
+main() {
+	int x = 2, y;
+	double d;
+
+	for (y = 2; y <= 6; y++)
+	{
+		d = power(x, y);
+		printf("%5d ^ %2d=%5.0f\n", x, y, d);	}
+}
