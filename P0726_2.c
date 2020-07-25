17ffb348554381ae44da6ffda1875a525b3a31f1
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-07-26 08:44:58 +0900

    Add files via upload

---
+++ P0726_2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+//#define SWAP(x,y,t)((t)=(x),(x)=(y),(y)=(t))
+
+struct a_tag {
+	char* x;
+	char* y;
+	struct a_tag* sp;
+
+
+}v[] = {
+	{"ABC", "DEF", v + 1},
+{ "GHI","JKL",v + 2 },
+{ "MNO","PQR",v } }
+;
+struct a_tag* ptr = v;
+
+;
+main() {
+	printf("%s\n", v[0].sp->x);
+	printf("%s\n", ptr->sp->y);
+	printf("%s\n", ptr->sp->sp->y);
+	printf("%s\n", ++(++ptr)->sp->x);
+}
