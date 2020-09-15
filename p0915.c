202181ac1e463cae45b069c6b348e873c68d462b
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-09-15 17:21:42 +0900

    Add files via upload

---
+++ p0915.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(void){
+char menu[][10]={
+"New","Save", "Save As", "Exit"
+};
+
+printf("%d", sizeof(menu));
+return 0;
+}
+
