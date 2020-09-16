7d261075c15a3e1a3e02c4c4f6ca9307b6a3f429
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-09-16 15:31:56 +0900

    Add files via upload

---
+++ p09155.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main(){
+    /*
+int i;
+int coin[6]={500,100,50,10,5,1};
+int count[6]={0};
+int input, rinput;
+input=714;
+rinput=input;
+for(i=0; i<6; i++)
+{
+    count[i]=rinput/coin[i];
+    rinput=rinput%coin[i];
+}
+for(i=0; i<6; i++)
+
+    printf("%d:%d\n", coin[i],count[i]);
+    return 0;
+
+*/
+char m[2][10]={"aaa","bbbbb","cc","fgh"};
+printf("%d",sizeof(m));
+}
