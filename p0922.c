149f74f2c78bbf6b82bb0cf3ceff5fbae6e3d243
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-11-22 21:15:02 +0900

    Add files via upload

--- p0922.c
+++ p0922.c
@@ -1,49 +1,51 @@
-#include<stdio.h>
-#include <stdlib.h>
-#define N 7
-static int ht[N];
+#include <stdio.h>
+#include<stdlib.h>
 
-hash_f(int a)
-{
-	return(a % 7);
-}
 
-hash_f2(int b)
-{
-	return(5 - (b % 5));
-}
 
-void add(int item)
-{
-	int i, hashvalue, inc;
-	i = hashvalue = hash_f															(item);
-	inc = hash_f2(item);
-	while (ht[i] != 0)
-	{
-		if (item == ht[i]){
-		printf("....\n");
-		exit(1);
-}
-i = (i + inc) % N;
-if (i == hashvalue) {
-	printf("...aaaa\n");
-	exit(1);
- }
+	int a[] = { 2,4,5,1,6,3,0 };
+
+	int* rcount(int* a, int low, int high, int d) {
+		int i;
+		int* p = malloc(d * sizeof(int));
+		for (i = 0; i < d; i++)
+			p[i] = 0;
+		for (i = low; i <= high; i++)
+
+			p[a[i] % d]++;
+		return p;
 	}
-	ht[i] = item;
-}
+
+	int linkcount(int s, int t) {
+		int nlinks = 0;
+		while (s != t) {
+			s = a[s];
+			nlinks++;
+		}
+		return nlinks;
+	}
+
+	int f(int x) {
+		static int id = 2;
+		id = a[id];
+		return id + x;
+	}
+
+	int g(int x) {
+		static int val = 3;
+		return val += x;
+	}
+
+	int main(void) {
+		int b[] = { 12,19,15,26,29, 21, 14 , 32, 17, 22, 23 };
+		int* p = rcount(b, 2, 8, 3);
+
+		printf("%d ", p[2]);
+		printf("%d ", linkcount(5, 6));
+		g(5);
+		printf("%d ", g(f(4)));
+		return 0;
+	}
 
 
-int main() {
-
-	int i;
-	add(8);
-	add(1);
-	add(9);
-	add(6);
-	add(13);
-	add(12);
-	for (i = 0; i < N; i++)
-		printf("%d ", ht[i]);
-}
 
