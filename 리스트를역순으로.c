2216e5ea7645d917447d459f36391ea33932ceee
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-11-30 08:00:23 +0900

    Add files via upload

---
+++ 리스트를역순으로.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef int element;
+
+typedef struct ListNode{
+    element data;
+    struct ListNode *link;
+}ListNode;
+
+ListNode* insert_first(ListNode *head, element value){
+    ListNode *p=(ListNode*)malloc(sizeof(ListNode));
+    p->data=value;
+    p->link=head;
+    head=p;
+    return head;
+ }
+
+void print_list(ListNode *head)
+{
+    ListNode *p;
+    for(p=head; p != NULL; p=p->link)
+            printf("%d->", p->data);
+    printf("NULL\n");}
+
+
+ListNode* reverse(ListNode *head)
+{
+
+    ListNode *p,*q,*r;
+    q=NULL;
+    p=head;
+
+    while(p != NULL){
+
+        r=q;
+        q=p;
+    p=p->link;
+    q->link=r;
+}
+return q;
+}
+
+int main(){
+
+    ListNode* head1=NULL;
+    ListNode* head2=NULL;
+
+    head1=insert_first(head1, 10);
+    head1=insert_first(head1, 20);
+    head1=insert_first(head1,30);
+
+    print_list(head1);
+
+    head2=reverse(head1);
+    print_list(head2);
+    return 0;
+}
+
+
+
+
+
+
+
+
