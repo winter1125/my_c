68dde891c77646b58b179d38cd5cf8fe02ba88e3
winter1125 <62247242+winter1125@users.noreply.github.com>
2020-12-28 20:53:23 +0900

    Add files via upload

--- main.c
+++ main.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#define max 5
 
-void  main(){
-   int i=3,j=5;
-   int *ip,*jp;
-   ip=&i;
-   jp=&j;
-   *ip+=5;
-   *jp=*ip-*jp;
-   jp=ip;
-   *ip*=*jp;
-   printf("%d %d\n",i,j);
-   printf("%d %d\n",*ip,*jp);
+int main()
+{
+    int n,a,b,c,count=0;
+    scanf("%d", &n);
+    for(int a=1; a<n; a++)
+    {
+        for(int b=a; b<n; b++){
+
+            for(int c=b; c<n; c++)
+            {
+                if(a+b+c==n && a+b>c)
+                {
+                    count++;
+                    printf("%d %d %d\n", a,b,c);
+                }
+            }
+
+        }
+        if(count==0)
+            printf("-1\n");
+
     }
-
-
+}
