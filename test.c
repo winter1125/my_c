5ce70fb9d6f0ebe537ea615a5166b343a3472889
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-01-04 10:03:38 +0900

    Add files via upload

--- test.c
+++ test.c
@@ -1,15 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int main()
 {
-    int a;
-    char s[10];
-   scanf("%s", s);
-   a=atoi(s);
-    printf("10진수 =%d\n", a);
-    printf("8진수 = %o\n", a);
-    printf("16진수 = %x\n", a);
+    int a,b,c,d;
+    int count=0;
+
+
+       scanf("%d %d %d %d", &a, &b , &c,&d);
+
+
+    //int count=0;
+    count=a+b+c+d;
+
+
+    printf("%d", count);
+  // int len;
+//   len=strlen(arr);
+
+   //printf("len=%d", len);
+//   int count=0;
+
+   /*
+   for(int i=0; i<len; i++)
+   {
+       count+=arr[i];
+   }
+
+   printf("%d ", count);
+
+*/
+
+
+   if(count >=4)
+      {
+          printf("윳");
+      }
+    else if(count >=3)
+    {
+        printf("걸");
+    }
+    else if(count >=2)
+    {
+        printf("개");
+    }
+    else
+    {
+        printf("도");
+    }
 
 }
