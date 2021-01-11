5acd0a8198031df8e18bc6853bd2484d93240ace
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-01-12 06:52:38 +0900

    Add files via upload

---
+++ p1012.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int data[3]={1,2,3};
+
+typedef struct pp{
+
+    char name[10];
+    float bb;
+};
+
+void main(){
+struct pp aa;
+printf("%d", sizeof(aa));
+char ch;
+ch=getchar();
+putchar(ch);
+}
