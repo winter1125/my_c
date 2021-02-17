43e30cfda3b529ce360f0dd0909c55b79bba7098
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-02-18 07:11:19 +0900

    Add files via upload

--- p0117.c
+++ p0117.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int recursive(int n)
+{
+
+
+    int sum=0;
+    if(n<0) return 0;
+    return n+recursive(n-1);
+}
+int main()
+{
+   int n;
+   n=100;
+   scanf("%d", &n);
+   printf("%d ", recursive(n));
+}
