4c565ec7ee139d970dc0114e025448c46749d83a
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-02-22 19:27:32 +0900

    Add files via upload

--- p0220_2.c
+++ p0220_2.c
@@ -1,41 +1,95 @@
 #include <stdio.h>
-#pragma warning(disable:4996)
+#include <stdlib.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#define MAX 100
+#define max(a,b)((a)>(b)?a:b)
+#define swap(a,b,t)((t)=(a),(a)=(b),(b)=(t))
 
 
-int n;
-char array[8];
+int fail[MAX];
 
-
-void func(int num)
+void failure(char* string)
 {
-    for (int i = 1; i <= 2; i++)
+
+    int n;
+    n = strlen(string);
+    int i, j;
+    fail[0] = -1;
+
+    for (j = 1; j < n; j++)
     {
-        if (i == 1) {
-            array[num] = 'O';
+        i = fail[j - 1];
+        while (string[j] != string[i + 1] && i >= 0)
+        {
+
+            i = fail[i];
         }
 
+        if (string[i + 1] == string[j])
+        {
+            fail[j] = i + 1;
+        }
         else
         {
-            array[num] = 'x';
-        }
-
-        if (num != n)
-            func(num + 1);
-
-        if (num == n)
-        {
-            for (int i = 1; i <= n; i++)
-                printf("%c", array[i]);
-            printf("\n");
+            fail[j] = -1;
         }
 
     }
 
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", fail[i]);
+
+    }
+
+    printf("\n");
+
 }
+
+
+int kmp(char* string, char* pat)
+{
+    int i = 0, j = 0;
+    int lens = strlen(string);
+    int lenp = strlen(pat);
+    while (i <= lens && j < lenp)
+    {
+        if (string[i] == pat[j])
+        {
+            i++;
+            j++;
+        }
+        else if (j == 0)
+        {
+            i++;
+
+        }
+        else
+        {
+
+            j = fail[i - 1] + 1;
+        }
+    }
+    return((j == lenp) ? i - lenp; -1);
+}
+
+
+
+
+
 
 int main()
 {
-    scanf("%d", &n);
-    func(1);
-}
+    int count = 0;
+    int flag = -1;
+    char string[] = "aabaacaababacaab";
+    char pat[] = "caab";
+    char* str = string;
 
+    failure(string);
+    printf("%d ", kmp(string, pat));
+    return 0;
+
+}
