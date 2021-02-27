96134564cdbb87fe59685416b6a1ce6e4ad0233a
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-02-27 21:24:28 +0900

    Add files via upload

---
+++ 문자열2.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define swap(x,y,t)((t)=(x),(x)=(y),(y)=(t))
+#define max(a,b)(((a)>(b))?(a):(b))
+
+int fail[100];
+
+void failure(char* pat)
+{
+	int i, j;
+	int n = strlen(pat);
+	fail[0] = -1;
+	for (j = 1; j < n; j++)
+	{
+		i = fail[j - 1];
+		while ((pat[j] != pat[i + 1] && (i >= 0)))
+		{
+			i = fail[i];
+		}
+		if (pat[j] == pat[i + 1])
+		{
+			pat[j] = i + 1;
+		}
+		else
+		{
+			fail[j] = -1;
+		}
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", fail[i]);
+	}
+	printf("\n");
+
+}
+
+int kmp(char* string, char* pat)
+{
+	int i = 0;
+	int j = 0;
+	int lens = strlen(string);
+	int lenp = strlen(pat);
+	while (i < lens && j < lenp) {
+
+		if (string[i] == pat[j])
+		{
+			i++;
+			j++;
+		}
+		else if (j == 0)
+		{
+			i++;
+		}
+		else
+
+		{
+			j = fail[j - 1] + 1;
+		}
+
+	}
+
+	return ((j == lenp) ? (i - lenp) : -1);
+}
+
+int main() {
+	char string[] = "aabaacaababa";
+	char pat[] = "caab";
+	failure(string);
+	printf("%d ", kmp(string, pat));
+	return 0;
+}
