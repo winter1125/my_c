43ec4730e0a959c0e68a300ff1c96539e24e9de9
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-03-17 09:52:48 +0900

    Add files via upload

--- p0317.c
+++ p0317.c
@@ -1,24 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 100
 
-int binary(int num1)
+typedef struct TreeNode{
+    int data;
+    struct TreeNode *left, *right;
+}TreeNode;
+
+
+TreeNode n1={1, NULL,NULL};
+TreeNode n2={4, NULL,NULL};
+TreeNode n3={'*', &n1, &n2};
+TreeNOde n4={16, NULL,NULL};
+TreeNode n5={25, NULL,NULL};
+TreeNode n6={'+', &n4, &n5};
+TreeNode n7={'+', &n3, &n6};
+TreeNode *exp=&n7;
+
+int eval(TreeNode *root)
 {
+    if(root==NULL)
+        return 0;
+    if(root->left == NULL && root->roght==NULL)
+        return root->data;
+   }
+   int op1=evaluate(root->left);
+   int op2=evaluate(root->right);
+   printf("%d %c %d", op1, root->data, op2);
+   switch(root->data)
+   {
+        case '+':
+            return op1+op2;
+        case '-':
+            return op1-op2;
+        case '*':
+            return op1*op2;
+        case '/':
+            retun op1/op2;
+   }
+   }
+   return 0;
+   }
 
-    if(num1<2)
-        printf("%d", num1);
-    else
-    {
-        binary(num1/2);
-        printf("%d", num1%2);
-            }
-            return 0;
-}
-void main(){
-    int num1;
-    scanf("%d", &num1);
 
-    binary(num1);
+   int main(void)
+   {
+       printf("%d\n", evaluate(exp));
+       return 0;
 
-
-
-}
+   }
