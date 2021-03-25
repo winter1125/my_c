a9a05057194e92918af6668ca7d0f2327de56651
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-03-25 11:04:27 +0900

    Add files via upload

--- p0325.c
+++ p0325.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 int main()
 {
-    int num1, num2;
-     int arr[100001];
-    int count2=0;
-    int m=0;
+    char arr[100];
+    scanf("%s",arr);
     int sum=0;
-    sum=arr[0];
-
-
-    freopen("input.txt","r", stdin);
-
-    scanf("%d %d", &num1, &num2);
-
-    for(int i=0; i<num1; i++)
+    int len=strlen(arr);
+   // printf("%d\n",len);
+    int i;
+    for(i=0;i<len/2;i++)
     {
-        scanf("%d", &arr[i]);
+        char temp=arr[i];
+        arr[i]=arr[strlen(arr)-i-1];
+        arr[strlen(arr)-i-1]=temp;
     }
 
 
-    for(int i=0; i<num1; i++)
+/*
+    for(i=0;i<len;i++)
     {
-        sum+=arr[i];
+        printf("%c",arr[i]);
+    }
 
-        if(sum==num2){
-            count2++;
-        }
+    printf("\n");
 
-        else if(sum >  num2){      {
-           while(sum > num2){
-           sum-=arr[m++];
-             }
-             if(sum==num2)
-                count2++;
-        }
-        }
+*/
+    if(arr[0]=='0')
+
+    {
+        for(i=0;arr[i]=='0';i++)
+        ;
+    while(i<strlen(arr))
+    {
+        printf("%c",arr[i]);
+        sum=sum+arr[i]-'0';
+        i++;
+    }
+
+    }
+    else{
+            for(i=0;i<len;i++){
+sum=sum+arr[i]-'0';
+    }
 
 }
-printf("%d", count2);
 
+printf("%d",sum);
 }
