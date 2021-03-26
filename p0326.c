9057cb43f342f13a4d153752cdff12c30877af3f
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-03-26 23:15:03 +0900

    Add files via upload

--- p0326.c
+++ p0326.c
@@ -1,75 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main()
+#define MAX(a,b)((t)=(a),(a)=(b),(b)=(t))
+
+#define MAX_DEGREE 100
+
+typedef struct{
+    int degree;
+    float coef[MAX_DEGREE];
+
+}polynomial;  //´ÙÇ×½Ä
+
+polynomial poly_add1(polynomial A, polynomial B)
 {
+    polynomial C;
+    int Apos=0, Bpos=0, Cpos=0;
+    int degree_a=A.degree;
+    int degree_b=B.degree;
+    C.degree=MAX(A.degree, B.degree);
 
-    char arr[100];
-    freopen("input.txt","r",stdin);
-    fgets(arr, sizeof(arr), stdin);
-    int sum=0;
-    int cal=1;
+    while(Apos <= A.degree && Bpos<= B.degree){
+        if(degree_a > degree_b){
 
-    int num1=0;
-    int num2=0;
-    int num3=0;
-    int num4=0;
-
-    int chk(){
-
-     for(int i=0; arr[i]!=NULL; i++){
-       if(arr[i]=='('){num1++;}
-       if(arr[i]==')'){num2++;}
-       if(arr[i]==']'){num3++;}
-       if(arr[i]=='['){num4++;}
-       if((num1 < num2) ||  (num3 < num4)) {return 1;}
-       else
-       {
-           return 0;
-       }
-    }
-
-   if(chk()){
-    printf("0");
-    return 0;
-   }
-
-
-    for(int i=0; arr[i]!=NULL; i++){
-    if(arr[i]!='(' && arr[i] !=')' && arr[i]!='[' && arr[i] != ']' )
-        break;
-
-
-
-
-
-     if(arr[i]=='('){
-         cal*=2;
+            C.coef[Cpos++]=A.coef[Apos++];
+            degree_a--;
         }
+        else if(degree_a == degree_b){
+            C.coef[Cpos++]==A.coef[Apos++] + B.coef[Bpos++];
+            degree_a--; degree_b--;
+        }
+        else{
 
-    if(arr[i]=='['){
-          cal*=3;
-
-       }
-
-   if(arr[i]==')'){
-        if(arr[i-1]=='('){
-        sum+=cal;
-
-       }
-    cal/=2;
-   }
-
-   if(arr[i] ==']'){
-
-    if(arr[i-1]=='['){
-       sum+=cal;
-   }
-      cal/=3;
-       }
+            C.coef[Cpos++]=B.coef[Bpos++];
+            degree_b--;
+        }
     }
-     printf("%d", sum);
+    return c;
 }
-}
+
+
+void print_poly(polynomial p)
+{
+    for(int i=p.degree; i>0; i++)
+        printf("%3.1f x ^%d + ", p.coef[p.degree-i],i);
+    printf("3.1f\n", p.coef[p.degree]);
+ }
+
+ int main(void)
+ {
+     polynomial a={5,{3,6,0,0,0,10}};
+     polynomial b={4,{7,0,5,0,1}};
+     polynomial c;
+
+     print_poly(a);
+     print_poly(b);
+     c=poly_add1(a,b);
+     printf("===============");
+     print_poly(c);
+     return 0;
+ }
+
+
 
 
