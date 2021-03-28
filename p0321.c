ff62904772f9cffc4a41c8bfbf3df39cc2c16ec6
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-03-28 11:11:12 +0900

    Add files via upload

--- p0321.c
+++ p0321.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int f(int a, int b)
+{
+    if (a <= 0)
+        return b;
+    else f(a - 1, b * 2) + b;
+}
+
 int main()
 {
-    struct s1 {
-        char c[5], * s;
-    }s1 = { "comp", "test" };
-
-    struct s2 {
-        char* cp;
-        struct s1  ss1;
-    } s2 = { "math", {"hong", "park"} };
-
-    printf("%c\t%c\n", s1.s[0], *s1.s);
-    printf("%s\t%s\n", s1.c, s1.s);
-    printf("%s\t%s\n", s2.cp, s2.ss1.s);
-    printf("%s\t%s\n", ++s2.cp,++s2.ss1.s);
+    printf("%3d\n", f(8, 2));
 }
