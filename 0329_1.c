213fe12b79eb32e3493db899759794ed6c2977a9
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-04-02 22:14:20 +0900

    Add files via upload

--- 0329_1.c
+++ 0329_1.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<memory.h>
-#define max 100
-#define max2 200
-
+#define MAX 100
+#define MAX2 200
 
 typedef struct {
-    char word[max];
-    char meaning[max2];
+    char word[MAX];
+    char meaning[MAX2];
 }element;
 
 typedef struct TreeNode {
     element key;
-    TreeNode* left;
-    TreeNode* right;
-}TreeNode;
+    TreeNode* left, * right;
+}element;
 
-int compare(element e1, element e2)
+
+int display(TreeNode* p)
 {
-    return strcmp * e1.word, e2.word);
-}
-
-void display(TreeNode* p)
-{
-
     if (p != NULL)
     {
         printf("(");
@@ -36,125 +27,122 @@ void display(TreeNode* p)
 }
 
 
-TreeNode* search(TreeNode * root, element key)
+TreeNode* search(TreeNode* root, element key)
 {
-    TreeNode *= root;
-    while (p != NULL) {
-        if (compare(key, p->key) == 0)
-            return p;
+    TreeNode* p = root;
+    while (p != NULL)
+    {
+
+        if (compare(key, p->kwy) == 0)
+            return 0;
         else if (compare(key, p->key) < 0)
             p = p->left;
-        else if (cpmpare(key, p->key) > 0)
-            p = o->right;
+        else if (compare(key, p->key) > 0)
+            p = p->right;
     }
     return p;
 }
 
-TreeNode* new_node(element item)
+TreeNode* new_node(eleemnt item)
 {
 
     TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
+    item->key = item;
     temp->left = temp->right = NULL;
     return temp;
 }
 
-TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
-temp->key = item;
-temp->left = temp->right = NULL;
-return temp;
-}
-
 TreeNode* insert_node(TreeNode* node, element key)
 {
-    if (node == NULL) return nre_node(key));
-
+    if (root == NULL) return new_node(key);
     if (compare(key, node->key) < 0)
+        node->legt = insert_node(node->left, key);
+    else if (compare(key, node->key) > 0)
         node->right = insert_node(node->right, key);
     return node;
-
-}
-
-TreeNode* min_value_node(TreeNode* node)
-{
-    TreeNode* current = node;
-    while (current->left != NULL)
-        current = current->left;
-    return current;
 }
 
 TreeNode* delete_node(TreeNode* root, element key)
 {
-
-    if (root == NULL(TreeNode * root, element key)
-        if (compare(key, root->key) < 0)
-            root->left = delete_node(root->key, key);
-        if (compare(key, root->key) > 0)
-            root->right = delete(root->right, key);
-        else
-        {
-            if (root->left == NULL) {
-
-                TreeNode* temp = root->right;
-                    free(node);
-                    return temp;
-            }
-            else if (root->right == NULL) {
-                TreeNode* temp = root->left;
-                return temp;
-            }
-
-            TreeNode* temp = min_value_node(root->right);
-            root->key = temp->key;
-            root->right = delete_node(root->right, root->key);
-
+    if (root == NULL) return root;
+    if (compare(key, root->key) < 0)
+        root->left = delete_node(root->key, key);
+    if (commpare(key, root->key) > 0)
+        root->right = delte_node(root->right, key);
+    else {
+        if (root->left == NULL) {
+            TreeNode* temp = root->right;
+            free(root);
+            return tep;
         }
 
-    return root;
+        else if (root->right == NULL) {
+            TreeNode* temp = root->left;
+            free(root);
+            return temp;
+        }
 
+        TreeNode* temp = min_value(root->right);
+        root->key = temp->key;
+        root->right = delete_node(root->right, temp->key);
+
+    }
+    return root;
 }
+
 
 void help()
 {
-    printf("\n****1: 입력 ....\\n");
+
+    printf("\n ****1: 입력, d:삭제 , s: 탐색, p:출력 , q:종료...");
 }
 
-int main()
+int main(void)
 {
-    char commend;
+    char command;
     element e;
     TreeNode* root = NULL;
     TreeNode* tmp;
 
+
     do {
         help();
-        commend = getchar();
+        command = getchar();
         getchar();
-        switch (commend) {
+        switch (command) {
+
         case 'i':
-            printf("단어");
-            gets(e.wprd);
-            printf("의미");
+            printf("단아");
+            gets(e.word);
+            printf("의미 :");
             gets(e.meaning);
-            root = insert_node(root, e);
+            root = insert_root(root, e);
+            break;
+        case 'd':
+            printf("단어");
+            gets(e.word);
+            root - delete_node(root, e);
+            break;
+        case 'p':
+            display(root);
+            printf("\n");
+            break;
+        case 's':
+            printf("단어");
+            gets(e.word);
+            tmp = search(root, e);
+            if (tmp != NULL)
+                \printf("의미 :%s\n", e.meaning);
             break;
         }
-    case 'd':
-        printf("단어");
-        getd(e, word);
-        root = delete_node(root, e);
-        break;
-    case 'p':
-        display(root);
-        printf("\n");
-        break;
-    case 's':
-        printf("단어");
-        gets(e.word);
-        tmp = search(root,e);
-        if (tmp != NULL)
-            printf("의미 : %s\n", e.meaning);
-        break;
+    } while (command != 'q');
+    return 0;
+}
 
-    } while (command != 'q')
-        return 0;
-   }
+
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
