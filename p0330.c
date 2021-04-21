a78092f9581a7bf5b8fc9d6a28edf8fc1f5a4ad2
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-04-21 22:20:40 +0900

    Add files via upload

--- p0330.c
+++ p0330.c
@@ -1,160 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<memory.h>
-#define MAX 100
-#define MAX2 200
+#define MAX 50
+typedef struct GraphType {
+    int n;
+    int adj_mat[MAX][MAX];
+}GraphType;
 
-
-typedef struct {
-    char word[MAX];
-    char meaning[MAX2];
-}element;
-
-typedef struct TreeNode {
-    element key;
-    TreeNode* left;
-    TreeNode* right;
-}TreeNode;
-
-int compare(element e1, element e2)
+void init(GraphType* g)
 {
-    return strcmp * e1.word, e2.word);
+
+    int r, c;
+    g->n = 0;
+    for (r = 0; r < MAX; r++)
+        for (c = 0; c < MAX; c++)
+            g->adj_mat[r][c] = 0;
 }
 
-void display(TreeNode* p)
+void insert_vertex(GraphType* g, int v)
 {
+    if (((g->n) + 1) > MAX) {
+        printf("overflow");
+        return;
+    }
+    g->n++;
+}
 
-    if (p != NULL)
+void insert_edges(GraphType* g, int start, int end)
+{
+    if (start >= g->n || end >= g->n)
     {
-        printf("(");
-        display(p->left);
-        printf("%d : %d", p->key.word, p->key.meaning);
-        display(p->right);
-        printf(")");
+        printf("그래프 : 정점번호 오류 ");
+        return;
     }
+    g->adj_mat[start][end] = 1;
+    g->adj_mat[end][start] = 1;
+
+
 }
 
 
-TreeNode* search(TreeNode* root, element key)
+void print_adj_mat(GraphType* g)
 {
-    TreeNode* P = root;
-    while (p != NULL) {
-        if (compare(key, p->key) == 0)
-            return p;
-        else if (compare(key, p->key) < 0)
-            p = p->left;
-        else if (cpmpare(key, p->key) > 0)
-            p = o->right;
-    }
-    return p;
-}
-
-TreeNode* new_node(element item)
-{
-
-    TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
-    temp->left = temp->right = NULL;
-    return temp;
-}
-
-TreeNode* temp = (TreeNode*)malloc(sizeof(TreeNode));
-temp->key = item;
-temp->left = temp->right = NULL;
-return temp;
-}
-
-TreeNode* insert_node(TreeNode* node, element key)
-{
-    if (node == NULL) return nre_node(key));
-
-    if (compare(key, node->key) < 0)
-        node->right = insert_node(node->right, key);
-    return node;
-
-}
-
-TreeNode* min_value_node(TreeNode* node)
-{
-    TreeNode* current = node;
-    while (current->left != NULL)
-        current = current->left;
-    return current;
-}
-
-TreeNode* delete_node(TreeNode* root, element key)
-{
-
-    if (root == NULL(TreeNode * root, element key)
-        if (compare(key, root->key) < 0)
-            root->left = delete_node(root->key, key);
-        if (compare(key, root->key) > 0)
-            root->right = delete(root->right, key);
-        else
-        {
-            if (root->left == NULL) {
-
-                TreeNode* temp = root->right;
-                    free(node);
-                    return temp;
-            }
-            else if (root->right == NULL) {
-                TreeNode* temp = root->left;
-                return temp;
-            }
-
-            TreeNode* temp = min_value_node(root->right);
-            root->key = temp->key;
-            root->right = delete_node(root->right, root->key);
-
+    for (int i = 0; i < g->n; i++)
+    {
+        for (int j = 0; j < g->n; j++) {
+            printf("%2d", g->adj_mat[i][j]);
         }
-
-    return root;
-
-}
-
-void help()
-{
-    printf("\n****1: 입력 ....\\n");
-}
-
-int main(void)
-{
-    char command;
-    element e;
-    TreeNode* root = NULL;
-    TreeNode* tmp;
-
-    do {
-        help();
-        command = getchar();
-        getchar();
-        switch (commend) {
-        case 'i':
-            printf("단어");
-            gets(e.wprd);
-            printf("의미");
-            gets(e.meaning);
-            root = insert_node(root, e);
-            break;
-        }
-    case 'd':
-        printf("단어");
-        getd(e.word);
-        root = delete_node(root, e);
-        break;
-    case 'p':
-        display(root);
         printf("\n");
-        break;
-    case 's':
-        printf("단어");
-        gets(e.word);
-        tmp = search(root, e);
-        if (tmp != NULL)
-            printf("의미 : %s\n", e.meaning);
-        break;
+    }
+}
 
-    } while (command != 'q')
-        return 0;
+
+int main()
+{
+    GraphType* g;
+    g = (GraphType*)malloc(sizeof(GraphType));
+    init(g);
+    for (int i = 0; i < 4; i++)
+        insert_vertex(g, i);
+    insert_edges(g, 0, 1);
+    insert_edges(g, 0, 2);
+    insert_edges(g, 0, 3);
+    insert_edges(g, 1, 2);
+    insert_edges(g, 2, 3);
+
+    print_adj_mat(g);
+    free(g);
+
+    //printf("Hello world!\n");
+    //return 0;
 }
