7689aa72c847b69263fdb50b5cb01ec386c231eb
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-04-26 19:55:46 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,22 +1,44 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 10
 
+void swap(int a[], int i, int j)
+{
 
-void main() {
-	char data[10] = { 1,2,3,4,5,6,7,8,9,10};
-
-
-	int len;
-	len = sizeof(data);
-	printf("%d \n", len);
-	char* p = data;
-	int sum = 0;
-	
-	for (int i = 0; i < len; i++)
-	{
-		sum = sum + *p;
-		p++;
-	}
-
-	printf("%d ", sum);
+    int t = a[i];
+    a[i] = a[j];
+    a[j]=t;
 }
+
+void sort(int list[], int n)
+{
+    int i, j, b = n - 1;
+    for (i = 0; i <= n - 1; i++) {
+        if (list[b] <= list[i])
+            b = i;
+        if (n == 7 && i == 0)
+            for (j = 0; j < MAX; j++)
+                printf("%d ", list[j]);
+    }
+    swap(list, n - 1, b);
+
+    if (n == 5)
+
+        for (j = 0; j < MAX; j++)
+            printf("%d  ", list[j]);
+
+    printf("\n");
+    if (n >= 1)
+        sort(list, n - 1);
+
+
+}
+
+
+
+main() {
+    int list[MAX] = {6,5,1,9,2,8,10,3,7,4};
+    sort(list, MAX);
+
+}
+ 
