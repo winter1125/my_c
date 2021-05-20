ef1622354828d92c180b8f3d7554fd037898b719
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-05-20 21:16:40 +0900

    Add files via upload

--- 소스.c
+++ 소스.c
@@ -1,44 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10
+#include<string.h>
 
-void swap(int a[], int i, int j)
+
+int swap(int* a, int* b)
 {
 
-    int t = a[i];
-    a[i] = a[j];
-    a[j]=t;
-}
-
-void sort(int list[], int n)
-{
-    int i, j, b = n - 1;
-    for (i = 0; i <= n - 1; i++) {
-        if (list[b] <= list[i])
-            b = i;
-        if (n == 7 && i == 0)
-            for (j = 0; j < MAX; j++)
-                printf("%d ", list[j]);
-    }
-    swap(list, n - 1, b);
-
-    if (n == 5)
-
-        for (j = 0; j < MAX; j++)
-            printf("%d  ", list[j]);
-
-    printf("\n");
-    if (n >= 1)
-        sort(list, n - 1);
-
-
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 
+int main() {
+    int t;
+    int a[7] = {4,5,2,1,3,0,6 };
+    for (int i = 0; i < 4; i++)
+        for (int j = i + 1; j < 7; j++)
+            if (a[i] > a[j])
+                swap(&a[i], &a[j]);
 
-main() {
-    int list[MAX] = {6,5,1,9,2,8,10,3,7,4};
-    sort(list, MAX);
+    for (int i = 0; i < 7; i++)
+        printf("%d  ", a[i]);
 
+       retuen 0;
 }
- 
