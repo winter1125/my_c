bc7551664604bbcd91581cc7cd883f02bfe759a7
winter1125 <62247242+winter1125@users.noreply.github.com>
2021-06-14 09:41:10 +0900

    Add files via upload

--- p0607_2.c
+++ p0607_2.c
@@ -2,21 +2,35 @@
 #include <stdlib.h>
 #define MAX(a,b)(((a)>(b))? (a): (b))
 #define MAX_DEGREE 101
+#define ROWS 3
+#define COLS 3
+
+void matrix_t(int A[RWS][COLS], int B[ROWS][COLS])
+{
+	for (int r = 0; r < ROWS; r++)
+		for (int c = 0; c < COLS; c++)
+			B[c][r] = A[r][c];
+}
+
+void matrix_p(int A[ROWS][COLS])
+{
+	printf("===================\n");
+	for (int r = 0; r < ROWS; r++) {
+		printf("%d ", A[r][c]);
+		printf("\n");
+	}
+	printf("==================\n");
+}
 
 
 int main(void)
 {
-	int a, b, cnt = 0;
-	int n = 10;
-	for(a=1; a<=n; a++)
-	{ 
-		for (b = a; b > 0; b--)
-		{
-			if (a% b == 0) cnt++;
-		}
-		if (cnt == 2) printf("%2d", a);
-		cnt = 0;
+	int array1[ROWS][COLS]; = { {2,3,0},{8,9,1},{7,0,5} };
+	int array2[ROWS][COLS];
+	int array2[ROWS][COLS];
 
-	}
+	matrix_t(array1, array2);
+	matrix_p(array1);
+	matrix_p(array2);
 	return 0;
-	}
+}
